bd629bf43fda96b6348882ea07d87a112a3f90a9
tancsrymb <ktomyamku@gmail.com>
2025-06-25 09:21:10 +0700

    V.1.1

---
+++ app/about/page.js
@@ -0,0 +1,23 @@
+import { Prompt } from 'next/font/google';
+
+const prompt = Prompt({
+  subsets: ['thai', 'latin'],
+  weight: ['400', '700'],
+});
+
+export const metadata = {
+  title: "About",
+};
+
+export default function Home() {
+  return (
+    <main className={`${prompt.className} min-h-screen flex items-center justify-center bg-white px-4`}>
+      <h1 className="text-4xl text-[#a1866f] tracking-wide text-center">
+        ภาษาไทย<br />
+        test<br/>
+        ทำไม
+      </h1>
+
+    </main>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,20 @@
+import { Prompt } from 'next/font/google';
+
+const prompt = Prompt({
+  subsets: ['thai', 'latin'],
+  weight: ['400', '700'],
+});
+
+export const metadata = {
+  title: "Home",
+};
+
+export default function Home() {
+  return (
+    <main className={`${prompt.className} min-h-screen flex items-center justify-center bg-white px-4`}>
+      <h1 className="text-4xl text-[#a1866f] tracking-wide text-center">
+        Mr.Jittiphat Junthakee
+      </h1>
+    </main>
+  );
+}
